0a72ed404f70acc3f4bf20623fd85ba6743f1209
Nick Meyer <nmeyer5435@gmail.com>
2022-05-19 15:23:50 -0500

    "Autocommit at 2022-05-19 15:23:49"

--- upload.sh
+++ upload.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+rm -rf ./dist/
 CSTRING="Autocommit at $(date '+%Y-%m-%d %H:%M:%S')"
 git add .
 git commit -m "\"$CSTRING\""
 git push origin
 python -m build --no-isolation 
-twine upload -r testpypi dist/*
-twine upload dist/*
+twine upload --skip-existing -r testpypi dist/*
+twine upload --skip-existing dist/*
+rm -rf ./dist/
